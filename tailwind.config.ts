3b5cf2cf05ffb17bf13f1c37e415ac95f7bfab2c
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2025-02-22 20:14:14 +0400

    upd

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,12 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class', // This is the correct type
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "#ffffff",
+        foreground: "#171717",
       },
     },
   },
